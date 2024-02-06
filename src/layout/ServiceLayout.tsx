b61f4109615bc63a602c119bf88c3efc64b53942
Jesusborjas006 <jesusbor006@gmail.com>
2024-02-06 13:22:44 -0600

    Add hover styling to all service cards

--- src/layout/ServiceLayout.tsx
+++ src/layout/ServiceLayout.tsx
@@ -7,43 +7,43 @@ const ServiceLayout = () => {
         <h2 className="text-4xl font-semibold">Our Services</h2>
 
         <div className="flex mt-12 gap-x-8">
-          <div className="border border-b-4 border-b-[#D7926B] w-[300px] px-6 py-8">
+          <div className="group border border-b-4 border-b-[#D7926B] w-[300px] px-6 py-8 hover:bg-[#18130C] hover:text-white">
             <h4 className="font-bold text-lg mb-4">Architecture & Interior</h4>
-            <p className="text-sm mb-6">
+            <p className="text-sm mb-6 text-[#615E5E] group-hover:text-white">
               Aenean commodo nec ligula eget cum sociis dolor.
             </p>
             <div className="flex items-center">
-              <div className="bg-black w-10 h-[2px] mr-2"></div>
+              <div className="bg-black w-10 h-[2px] mr-2 group-hover:bg-white"></div>
               <p>READ MORE</p>
             </div>
           </div>
-          <div className="border border-b-4 border-b-[#D7926B] w-[300px] px-6 py-8">
-            <h4 className="font-bold text-lg mb-4">Architecture & Interior</h4>
-            <p className="text-sm mb-6">
+          <div className="group border border-b-4 border-b-[#D7926B] w-[300px] px-6 py-8 hover:bg-[#18130C] hover:text-white">
+            <h4 className="font-bold text-lg mb-4">Project Planning</h4>
+            <p className="text-sm mb-6 text-[#615E5E] group-hover:text-white">
               Aenean commodo nec ligula eget cum sociis dolor.
             </p>
             <div className="flex items-center">
-              <div className="bg-black w-10 h-[2px] mr-2"></div>
+              <div className="bg-black w-10 h-[2px] mr-2 group-hover:bg-white"></div>
               <p>READ MORE</p>
             </div>
           </div>
-          <div className="border border-b-4 border-b-[#D7926B] w-[300px] px-6 py-8">
-            <h4 className="font-bold text-lg mb-4">Architecture & Interior</h4>
-            <p className="text-sm mb-6">
+          <div className="group border border-b-4 border-b-[#D7926B] w-[300px] px-6 py-8 hover:bg-[#18130C] hover:text-white">
+            <h4 className="font-bold text-lg mb-4">Product Design</h4>
+            <p className="text-sm mb-6 text-[#615E5E] group-hover:text-white">
               Aenean commodo nec ligula eget cum sociis dolor.
             </p>
             <div className="flex items-center">
-              <div className="bg-black w-10 h-[2px] mr-2"></div>
+              <div className="bg-black w-10 h-[2px] mr-2 group-hover:bg-white"></div>
               <p>READ MORE</p>
             </div>
           </div>
-          <div className="border border-b-4 border-b-[#D7926B] w-[300px] px-6 py-8">
-            <h4 className="font-bold text-lg mb-4">Architecture & Interior</h4>
-            <p className="text-sm mb-6">
+          <div className="group border border-b-4 border-b-[#D7926B] w-[300px] px-6 py-8 hover:bg-[#18130C] hover:text-white">
+            <h4 className="font-bold text-lg mb-4">Custom Solutions</h4>
+            <p className="text-sm mb-6 text-[#615E5E] group-hover:text-white">
               Aenean commodo nec ligula eget cum sociis dolor.
             </p>
             <div className="flex items-center">
-              <div className="bg-black w-10 h-[2px] mr-2"></div>
+              <div className="bg-black w-10 h-[2px] mr-2 group-hover:bg-white"></div>
               <p>READ MORE</p>
             </div>
           </div>
