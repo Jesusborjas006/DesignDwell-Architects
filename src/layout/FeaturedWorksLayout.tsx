6ce59c32cbade7d1dbc1a5d2e912b62ec5ec1cdd
Jesusborjas006 <jesusbor006@gmail.com>
2024-02-07 11:24:25 -0600

    Add sub heading for featured projects section

---
+++ src/layout/FeaturedWorksLayout.tsx
@@ -0,0 +1,13 @@
+const FeaturedWorksLayout = () => {
+  return (
+    <section className="px-6 mt-24 w-[80%] md:mx-auto">
+      <div className="flex items-center mb-4">
+        <div className="bg-[#D7926B] w-14 h-[2px] mr-4"></div>
+        <h4 className="text-[#D7926B]">PROJECTS</h4>
+      </div>
+      <h2 className="text-4xl font-semibold">Featured Works</h2>
+    </section>
+  );
+};
+
+export default FeaturedWorksLayout;
