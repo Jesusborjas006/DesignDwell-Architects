a98f6de8efa70f09cebebf8e27abdb6a053d31dc
Jesusborjas006 <jesusbor006@gmail.com>
2024-02-07 11:30:49 -0600

    Add project navigation content

--- src/layout/FeaturedWorksLayout.tsx
+++ src/layout/FeaturedWorksLayout.tsx
@@ -1,11 +1,21 @@
 const FeaturedWorksLayout = () => {
   return (
     <section className="px-6 mt-24 w-[80%] md:mx-auto">
-      <div className="flex items-center mb-4">
-        <div className="bg-[#D7926B] w-14 h-[2px] mr-4"></div>
-        <h4 className="text-[#D7926B]">PROJECTS</h4>
+      <div className="flex items-end justify-between">
+        <div>
+          <div className="flex items-center mb-4">
+            <div className="bg-[#D7926B] w-14 h-[2px] mr-4"></div>
+            <h4 className="text-[#D7926B]">PROJECTS</h4>
+          </div>
+          <h2 className="text-4xl font-semibold">Featured Works</h2>
+        </div>
+        <div className="hidden lg:flex items-center space-x-4">
+          <p>PREV</p>
+          <div className="bg-black w-20 h-[2px] mr-4"></div>
+          <div className="bg-[#D7926B] w-20 h-[2px] mr-4"></div>
+          <p className="text-[#D7926B]">NEXT</p>
+        </div>
       </div>
-      <h2 className="text-4xl font-semibold">Featured Works</h2>
     </section>
   );
 };
